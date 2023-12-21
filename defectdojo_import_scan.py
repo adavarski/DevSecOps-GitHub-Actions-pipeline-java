999d3626db5c8c9cf9522267bd510818f19b1ba4
A.Davarski <42774466+adavarski@users.noreply.github.com>
2023-12-21 12:34:09 +0200

    Add files via upload

---
+++ defectdojo_import_scan.py
@@ -0,0 +1,42 @@
+import requests
+import sys
+import os
+
+
+def uploadToDefectDojo(is_new_import, token, url, product_name, engagement_name, filename):
+    multipart_form_data = {
+        'file': (filename, open(filename, 'rb')),
+        'scan_type': (None, 'SARIF'),
+        'product_name': (None, product_name),
+        'engagement_name': (None, engagement_name),
+    }
+
+    endpoint = '/api/v2/import-scan/' if is_new_import else '/api/v2/reimport-scan/'
+    r = requests.post(
+        url + endpoint,
+        files=multipart_form_data,
+        headers={
+            'Authorization': 'Token ' + token,
+        }
+    )
+    if r.status_code != 200:
+        sys.exit(f'Post failed: {r.text}')
+    print(r.text)
+
+
+
+if __name__ == "__main__":
+    try:
+        token = os.getenv("DEFECT_DOJO_API_TOKEN")
+    except KeyError: 
+        print("Please set the environment variable DEFECT_DOJO_API_TOKEN") 
+        sys.exit(1)
+    if len(sys.argv) == 9:
+        url = sys.argv[2]
+        product_name = sys.argv[4]
+        engagement_name = sys.argv[6]
+        report = sys.argv[8]
+        uploadToDefectDojo(False, token, url, product_name, engagement_name, report)
+    else:
+        print(
+            'Usage: python3 defectdojo_import_scan.py --host DOJO_URL --product PRODUCT_NAME --engagement ENGAGEMENT_NAME --report REPORT_FILE')
